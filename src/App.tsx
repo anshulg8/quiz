3855c9440edb6105c44b8b12d951b8784521dcdd
Anshul Garg <anshul@haveignition.com>
2024-08-28 15:01:09 +0530

    adds navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Button, Show } from '@chakra-ui/react'
 import { Grid, GridItem } from '@chakra-ui/react'
+import NavBar from './components/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,7 +17,9 @@ function App() {
         base: `"nav" "main"`,
         lg: `"nav nav" "aside main"`
       }}>
-        <GridItem area='nav' bg='coral'>Nav</GridItem>
+        <GridItem area='nav' bg=''>
+          <NavBar />
+        </GridItem>
         <Show above='lg'>
           <GridItem area='aside' bg='gold'>Aside</GridItem>
         </Show>
